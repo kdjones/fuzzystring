41f4a86b6425d91839aa7538e93377d3d873ad45
Moxh3 <11796171+Moxh3@users.noreply.github.com>
2021-08-22 13:00:04 +1000

    Update ApproximatelyEquals.cs
    Simplified using MatchingScore

--- FuzzyString/ApproximatelyEquals.cs
+++ FuzzyString/ApproximatelyEquals.cs
@@ -10,88 +10,16 @@ namespace FuzzyString
     { 
         public static bool ApproximatelyEquals(this string source, string target,  FuzzyStringComparisonTolerance tolerance, params FuzzyStringComparisonOptions[] options)
         {
-            List<double> comparisonResults = new List<double>();
+            double score = this.MatchingScore(target, tolerance, options)
 
-            if (!options.Contains(FuzzyStringComparisonOptions.CaseSensitive))
-            {
-                source = source.Capitalize();
-                target = target.Capitalize();
-            }
-
-            // Min: 0    Max: source.Length = target.Length
-            if (options.Contains(FuzzyStringComparisonOptions.UseHammingDistance))
-            {
-                if (source.Length == target.Length)
-                {
-                    comparisonResults.Add(source.HammingDistance(target) / target.Length);
-                }
-            }
-
-            // Min: 0    Max: 1
-            if (options.Contains(FuzzyStringComparisonOptions.UseJaccardDistance))
-            {
-                comparisonResults.Add(source.JaccardDistance(target));
-            }
-
-            // Min: 0    Max: 1
-            if (options.Contains(FuzzyStringComparisonOptions.UseJaroDistance))
-            {
-                comparisonResults.Add(source.JaroDistance(target));
-            }
-
-            // Min: 0    Max: 1
-            if (options.Contains(FuzzyStringComparisonOptions.UseJaroWinklerDistance))
-            {
-                comparisonResults.Add(source.JaroWinklerDistance(target));
-            }
-
-            // Min: 0    Max: LevenshteinDistanceUpperBounds - LevenshteinDistanceLowerBounds
-            // Min: LevenshteinDistanceLowerBounds    Max: LevenshteinDistanceUpperBounds
-            if (options.Contains(FuzzyStringComparisonOptions.UseNormalizedLevenshteinDistance))
-            {
-                comparisonResults.Add(Convert.ToDouble(source.NormalizedLevenshteinDistance(target)) / Convert.ToDouble((Math.Max(source.Length, target.Length) - source.LevenshteinDistanceLowerBounds(target))));
-            }
-            else if (options.Contains(FuzzyStringComparisonOptions.UseLevenshteinDistance))
-            {
-                comparisonResults.Add(Convert.ToDouble(source.LevenshteinDistance(target)) / Convert.ToDouble(source.LevenshteinDistanceUpperBounds(target)));
-            }
-
-            if (options.Contains(FuzzyStringComparisonOptions.UseLongestCommonSubsequence))
-            {
-                comparisonResults.Add(1 - Convert.ToDouble((source.LongestCommonSubsequence(target).Length) / Convert.ToDouble(Math.Min(source.Length, target.Length))));
-            }
-
-            if (options.Contains(FuzzyStringComparisonOptions.UseLongestCommonSubstring))
-            {
-                comparisonResults.Add(1 - Convert.ToDouble((source.LongestCommonSubstring(target).Length) / Convert.ToDouble(Math.Min(source.Length, target.Length))));
-            }
-
-            // Min: 0    Max: 1
-            if (options.Contains(FuzzyStringComparisonOptions.UseSorensenDiceDistance))
-            {
-                comparisonResults.Add(source.SorensenDiceDistance(target));
-            }
-
-            // Min: 0    Max: 1
-            if (options.Contains(FuzzyStringComparisonOptions.UseOverlapCoefficient))
-            {
-                comparisonResults.Add(1 - source.OverlapCoefficient(target));
-            }
-
-            // Min: 0    Max: 1
-            if (options.Contains(FuzzyStringComparisonOptions.UseRatcliffObershelpSimilarity))
-            {
-                comparisonResults.Add(1 - source.RatcliffObershelpSimilarity(target));
-            }
-
-            if (comparisonResults.Count == 0)
+            if (score == 0)
             {
                 return false;
             }
 
             if (tolerance == FuzzyStringComparisonTolerance.Strong)
             {
-                if (comparisonResults.Average() < 0.25)
+                if (score < 0.25)
                 {
                     return true;
                 }
@@ -102,7 +30,7 @@ namespace FuzzyString
             }
             else if (tolerance == FuzzyStringComparisonTolerance.Normal)
             {
-                if (comparisonResults.Average() < 0.5)
+                if (score < 0.5)
                 {
                     return true;
                 }
@@ -113,7 +41,7 @@ namespace FuzzyString
             }
             else if (tolerance == FuzzyStringComparisonTolerance.Weak)
             {
-                if (comparisonResults.Average() < 0.75)
+                if (score < 0.75)
                 {
                     return true;
                 }
@@ -124,7 +52,7 @@ namespace FuzzyString
             }
             else if (tolerance == FuzzyStringComparisonTolerance.Manual)
             {
-                if (comparisonResults.Average() > 0.6)
+                if (score > 0.6)
                 {
                     return true;
                 }
